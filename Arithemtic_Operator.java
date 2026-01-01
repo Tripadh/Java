73db3b18be0146e202d01d1279ef8b7b2ddaec58
Tripadh <vtripadh@gmail.com>
2026-01-01 19:41:21 +0530

    operators

---
+++ Arithemtic_Operator.java
@@ -0,0 +1,10 @@
+public class Arithemtic_Operator {
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        System.out.println("Addition: " + (a + b));
+        System.out.println("Subtraction: " + (b - a));
+        System.out.println("Multiplication: " + (a * b));
+        System.out.println("Division: " + (b / a));
+    }
+}
---
+++ Logical_operator.java
@@ -0,0 +1,11 @@
+public class Logical_operator {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = 30;
+
+        System.out.println("Logical AND (a < b && b < c): " + (a < b && b < c));
+        System.out.println("Logical OR (a > b || b < c): " + (a > b || b < c));
+        System.out.println("Logical NOT (!(a < b)): " + (!(a < b)));
+    }
+}
