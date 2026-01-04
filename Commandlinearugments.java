f7df582425e33cd733a8bf6d6783242cd20b98dd
Tripadh <vtripadh@gmail.com>
2026-01-04 11:30:17 +0530

    methods

---
+++ Commandlinearugments.java
@@ -0,0 +1,5 @@
+public class Commandlinearugments {
+    public static void main(String[] args) {
+        System.out.println("Command-line arguments:"+args[0]);
+}
+}
---
+++ method.java
@@ -0,0 +1,15 @@
+    class method  {
+
+    int add(int x, int y) {
+        int z = x + y;
+        return z;
+    }
+
+    public static void main(String[] args) {
+        method ob = new method(     );
+        int result = ob.add(5, 10);
+        System.out.println(result);
+    }
+}
+
+
