8f86e0a25879845af90798f3c61080322c0d9b13
Tripadh <vtripadh@gmail.com>
2026-01-06 12:10:35 +0530

    dyamnic and ArrayCopy

---
+++ ArrayCopy.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class ArrayCopy {
+    public static void main(String[] args) {
+        int a1[] = new int[10];
+        int a2[] = new int[a1.length];
+
+        Scanner s = new Scanner(System.in);
+
+        for (int i = 0; i < a1.length; i++) {
+            System.out.println("Enter value at index " + i + ":");
+            a1[i] = s.nextInt();
+        }
+
+        // copy a1 into a2
+        for (int i = 0; i < a1.length; i++) {
+            a2[i] = a1[i];
+        }
+
+        System.out.println("Array 1 Elements:");
+        for (int element : a1) {              // enhanced for loop
+            System.out.print(element + "\t");
+        }
+        System.out.println();
+
+        System.out.println("Array 2 Elements:");
+        for (int element : a2) {              // enhanced for loop
+            System.out.print(element + "\t");
+        }
+        System.out.println();
+
+        s.close();
+    }
+}
---
+++ DyamicArray.java
@@ -0,0 +1,66 @@
+
+    
+import java.util.ArrayList;
+import java.util.Scanner;
+public class DyamicArray {
+    public static void main(String[] args) {
+        ArrayList<Integer> a = new ArrayList<Integer>();
+        Scanner s = new Scanner(System.in);
+
+        char ch;
+
+        do {
+            System.out.println("*** Dynamic Array ***");
+            System.out.println("Choose any option:");
+            System.out.println("1. Insert element into array");
+            System.out.println("2. Remove element from array");
+            System.out.println("3. Clear all elements of an array");
+            System.out.println("4. Do you want to know size?");
+            System.out.println("5. Print elements of the dynamic array");
+
+            int option = s.nextInt();
+
+            switch (option) {
+                case 1:
+                    System.out.println("Enter element to insert:");
+                    int ele = s.nextInt();
+                    a.add(ele);
+                    break;
+
+                case 2:
+                    System.out.println("Enter index of the element to remove:");
+                    int index = s.nextInt();
+                    if (index >= 0 && index < a.size()) {
+                        a.remove(index);
+                    } else {
+                        System.out.println("Invalid index");
+                    }
+                    break;
+
+                case 3:
+                    a.clear();
+                    System.out.println("Array cleared");
+                    break;
+
+                case 4:
+                    System.out.println("Size of the array is: " + a.size());
+                    break;
+
+                case 5:
+                    System.out.println("Elements of the dynamic array:");
+                    System.out.println(a);
+                    break;
+
+                default:
+                    System.out.println("Invalid option chosen");
+            }
+
+            System.out.println("Do you want to continue (Y/y or N/n)?");
+            ch = s.next().charAt(0);
+
+        } while (ch == 'Y' || ch == 'y');
+
+        s.close();
+    }
+}
+
